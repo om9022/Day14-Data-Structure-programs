0a75fccd21bed26a3f81895ac8eaa95eb91f8fe9
om9022 <omkarchikane607@gmail.com>
2021-07-17 17:23:38 +0530

    Modified : Changed input values

--- src/com/bridgelabz/queue/QueueMain.java
+++ src/com/bridgelabz/queue/QueueMain.java
@@ -1,5 +1,14 @@
 package com.bridgelabz.queue;
 
 public class QueueMain {
+	public static void main(String[] args) 
+	{
+		QueueService<Integer> queue = new QueueService<Integer>();
+		queue.enqueue(56);
+		queue.enqueue(30);
+		queue.enqueue(70);
+		queue.print();
+		
+	}
 
 }
