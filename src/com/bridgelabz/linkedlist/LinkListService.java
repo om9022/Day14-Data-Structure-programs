addfd5c10ba9dc05775c5de8c4e3a8ec1d49bad8
om9022 <omkarchikane607@gmail.com>
2021-07-17 10:56:19 +0530

    Added : Method to insert node between two nodes

--- src/com/bridgelabz/linkedlist/LinkListService.java
+++ src/com/bridgelabz/linkedlist/LinkListService.java
@@ -2,6 +2,7 @@ package com.bridgelabz.linkedlist;
 
 public class LinkListService {
 	
+
 	LinkList head;
 
 	public void add(int data) 
@@ -30,6 +31,36 @@ public class LinkListService {
 			}
 			currentNode.next = newNode;
 		}
+	}
+	
+	public void insertAfter(int afterNodeData , int data)
+	{
+		LinkList newNode = new LinkList();
+		newNode.data = data;
+		newNode.next = null;
+		if (head == null) 
+		{			
+			this.head = newNode;
+		}
+		else
+		{
+			LinkList currentNode = head;
+			while (currentNode.data != afterNodeData && currentNode.next != null)
+			{
+				currentNode = currentNode.next;
+			}
+			if(currentNode.data != afterNodeData)
+			{
+				System.out.println(afterNodeData+" Not found ");
+			}
+			else 
+			{				
+				newNode.next = currentNode.next;
+				currentNode.next = newNode;
+			}
+
+		}
+		
 	}
 	
 	public void print()
