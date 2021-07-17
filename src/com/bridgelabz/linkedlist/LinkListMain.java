2022b447ca35d445255eb4cae09f6684427c5183
om9022 <omkarchikane607@gmail.com>
2021-07-17 10:47:55 +0530

    Added : Ability to add nodes in linklist

--- src/com/bridgelabz/linkedlist/LinkListMain.java
+++ src/com/bridgelabz/linkedlist/LinkListMain.java
@@ -4,8 +4,9 @@ public class LinkListMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to LinkList");
 		LinkListService linkList = new LinkListService();
-		linkList.add(56);
-		linkList.add(30);
 		linkList.add(70);
+		linkList.add(30);
+		linkList.add(56);
+		linkList.print();
 	}
 }
--- src/com/bridgelabz/linkedlist/LinkListService.java
+++ src/com/bridgelabz/linkedlist/LinkListService.java
@@ -2,13 +2,25 @@ package com.bridgelabz.linkedlist;
 
 public class LinkListService {
 	
+
+
 	LinkList head;
 
-	public void add(int data) {
+	public void add(int data) 
+	{
 		LinkList newNode = new LinkList();
 		newNode.data = data;
-		newNode.next = null;
 		newNode.next = head;
-		head = newNode;
+		this.head = newNode;
+	}
+	public void print()
+	{
+		LinkList tempNode = head;
+		while(tempNode.next != null)
+		{
+			System.out.print(tempNode.data+" -> ");
+			tempNode = tempNode.next;
+		}
+		System.out.print(tempNode.data);
 	}
 }
